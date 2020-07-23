6665ad0020818ca18d20cd5f30c4e73c00bf0e1f
gcastagnin <68518175+gcastagnin@users.noreply.github.com>
2020-07-23 17:53:06 -0300

    V 1.00
    Hace todo, puse algo de estética.
    No pude evitar una script js en el html que oculta una label

--- app.js
+++ app.js
@@ -4,79 +4,132 @@ let guardarTarea = function(){
 	let estado = document.querySelectorAll('#guardarTarea')
 	estado = estado[0]
 	if (estado.innerText == "Guardar tarea"){
-		console.log("Guardar")
-	let tareaId = generarId()
-	let textoTarea = document.querySelectorAll("#textoTarea")
-	textoTarea = textoTarea[0]
-	let vuelta = ingresarTextoTarea(tareaId, textoTarea.value)
-
+		let tareaId = generarId()
+		let textoTarea = document.querySelectorAll("#textoTarea")
+		textoTarea = textoTarea[0]
+		if (textoTarea.value == ""){
+			alert("Tarea vacía!")
+			return
+		}
+		let vuelta = ingresarTextoTarea(tareaId, textoTarea.value)
 	}
-	else {console.log("Editar")}
+	else {
+		console.log("Editar")
+		let textoTarea = document.querySelectorAll("#textoTarea")
+		textoTarea = textoTarea[0]
+		if (textoTarea.value == ""){
+			alert("Tarea vacía!")
+			return
+		}
 
+		let noVisible = document.querySelectorAll("#recId")
+		noVisible = noVisible[0]
+
+
+		let vuelta = editarTextoTarea(noVisible.innerText, textoTarea.value)
+
+		let textoBoton = document.querySelectorAll("#guardarTarea")
+		textoBoton = textoBoton[0]
+		textoBoton.innerText = "Guardar tarea"
+	}
 }
 
 
-let generarId =function () {
+let generarId = function () {
 
-	let v 	= 4
-	let randomId = ''
+	let v 	= 2
+	let randomId = 'A' //Los id de los nodos no puden comenzar con numeros!!
 	for (let i = v; i >= 0; i--) {
 		if(i == 0){
 			randomId += v0()
 		} else {
-			randomId += v0() + "-"
+			randomId += v0() + ""
 		}
 	}
 	return(randomId)
-
 }
 
 
 let v0 = function () {
 	let idDiccionario = []
 		let hexadecimal = dicHexadecimal()
-
 		for (var i = hexadecimal.length - 1; i >= 0; i--) {
 		 	let diccionarioSize = idDiccionario.push (hexadecimal[i])
 		 } 
-
 	let RandN 	= 0
 	let psw 	= ''
 	let lg 		= 4 // Evitar numeros negativos
 
 	for (let i = 0; i < lg; i++) {
 		psw += idDiccionario[Math.floor(Math.random() * idDiccionario.length)]
-			}
+	}
 		return psw;
 }
+
 
 let dicHexadecimal = function () {
 	let hexadeciMayusculas 	= [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'A', 'B', 'C', 'D', 'E', 'F']
 	return(hexadeciMayusculas)
 }
 
-let ingresarTextoTarea = function (tareaId, textoTarea) {
 
-	console.log(tareaId, textoTarea)
+let ingresarTextoTarea = function (tareaId, textoTarea) {
 	let listaTareas = document.querySelectorAll("#lista")
 	listaTareas = listaTareas[0]
-	template = `<li id="#${tareaId}">
-				${textoTarea}
-				<ul>
-				<li><button type="button" onclick="editarTarea(${tareaId})">Editar tarea</button> <button type="button" onclick="eliminarTarea(${tareaId})">Eliminar tarea</button>
-				</li>
-				</ul>
-				</li>`
+	template = `<li id="${tareaId}">
+				<span class="resaltado">${textoTarea}</span>
+					<ul>
+						<li>
+							<button type="button" onclick="editarTarea('${tareaId}')">Editar tarea</button> <button type="button" onclick="eliminarTarea('${tareaId}')">Eliminar tarea</button>
+						</li>
+					</ul>
+				</li><br id="br${tareaId}">`
 	listaTareas.innerHTML = listaTareas.innerHTML + template
 }
 
 
-let editarTarea = function(tareaId){
-	
+let editarTextoTarea = function(tareaId, textoTarea) {
+	let tarea = document.querySelectorAll("#" + tareaId)
+	tarea = tarea[0]
+	template = `<li id="${tareaId}">
+				<span class="resaltado">${textoTarea}</span>
+					<ul>
+						<li>
+							<button type="button" onclick="editarTarea('${tareaId}')">Editar tarea</button> <button type="button" onclick="eliminarTarea('${tareaId}')">Eliminar tarea</button>
+						</li>
+					</ul>
+				</li>`
+	tarea.innerHTML = template
 }
-//Al dar click en un editar voy a cambiar el texto del boton guardar por editar,
-// asi se si debo generar un id nuevo
-//o copiar el que guarde al dar editar en un input hidden
-// <li id="1">Coffee <button type="button" onclick="guardarTarea()">Guardar tarea</button> <button type="button" onclick="guardarTarea()">Guardar tarea</button></li>
-// <li id="2">Tea <button type="button" onclick="guardarTarea()">Guardar tarea</button> <button type="button" onclick="guardarTarea()">Guardar tarea</button></li>
-// <li id="3">Milk <button type="button" onclick="guardarTarea()">Guardar tarea</button> <button type="button" onclick="guardarTarea()">Guardar tarea</button></li>
+
+
+let eliminarTarea = function(tareaId){
+	let tarea = document.querySelectorAll("#" + tareaId)
+	tarea = tarea[0]
+	tarea.outerHTML = ""
+
+	tarea = document.querySelectorAll("#br" + tareaId)
+	tarea = tarea[0]
+	tarea.remove()
+}
+
+
+let editarTarea = function(tareaId){
+	let tarea = document.querySelectorAll("#" + tareaId)
+	tarea = tarea[0]
+	
+	let textoAEditar = document.querySelectorAll("#textoTarea")
+	textoAEditar = textoAEditar[0]
+
+	let noVisible = document.querySelectorAll("#recId")
+	noVisible = noVisible[0]
+	noVisible.innerText = tareaId
+
+	let textoBoton = document.querySelectorAll("#guardarTarea")
+	textoBoton = textoBoton[0]
+	textoBoton.innerText = "Editar"
+
+
+	let stringAQuitar = "Editar tarea Eliminar tarea"
+	textoAEditar.value = tarea.innerText.substr(0, (tarea.innerText.length-stringAQuitar.length))
+}
